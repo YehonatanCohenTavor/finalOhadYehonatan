bb68dc3945b195786d8bf2827d946c47854a33c9
hilma <jonathanco999@gmail.com>
2023-01-16 12:09:28 +0200

    more dynamicfunctions

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,14 +3,13 @@ var router = express.Router();
 const fs = require('fs');
 const { adjustUserPath } = require('../middlewares');
 
-const dynamicRoutes = [];
 
 // /* GET All files. */
-router.get('/:username',adjustUserPath, function (req, res, next) {
+router.get('/:username', adjustUserPath, function (req, res, next) {
   let userFiles = [];
   console.log(res.locals.path);
   fs.readdir(res.locals.path, (err, filesNames) => {
-    filesNames.forEach((file,index) => {
+    filesNames.forEach((file, index) => {
       fs.stat(res.locals.path + `/${file}`, (err, stat) => {
         userFiles.push({
           name: file,
@@ -19,56 +18,88 @@ router.get('/:username',adjustUserPath, function (req, res, next) {
           size: stat.size
         })
         index === filesNames.length - 1 && res.json(userFiles);
-      }) 
+      })
     })
   });
 });
 
 //GET specific files
-router.get('/:username/:file', adjustUserPath, (req, res) => {
-  fs.readFile(`${res.locals.path}/${req.params.file}`,'utf8', (err, data) => {
+router.get('/:username/:folder?/:file', adjustUserPath, (req, res) => {
+  fs.readFile(`${res.locals.path}/${(req.params.folder) || ''}/${req.params.file}`, 'utf8', (err, data) => {
+    if (err) console.log(err);
     res.send(data);
   })
 })
 
 //POST a new file/directory
 
-router.post(`/:username`,adjustUserPath, ({ body }, res) => {
-  if (body.type === 'file') {
-    fs.appendFile(`${res.locals.path + '/' + body.name}`, `${ body.data }`, err => {
+router.post(`/:username/:folder?`, adjustUserPath, (req, res) => {
+  let path = `${res.locals.path}/${req.params.folder || ''}/${req.body.name}`;
+  if (req.body.intention === 'copy') {
+    if (req.body.type === 'file') {
+      fs.copyFile(path, path + '-copy', (err) => {
+        if (err) console.log(err);
+        console.log("file copied");
+        res.json(req.body);
+      })
+    } else {
+      fs.mkdir(path + '-copy', err => {
+        if (err) console.log(err);
+        fs.readdir(path, (err, fileNames) => {
+          fileNames.forEach((file, index) => {
+            fs.copyFile(`${path}/${file}`, `${path + '-copy'}/${file}-copy`, err => {
+              if (err) console.log(err);
+              index === fileNames.length - 1 && res.json(req.body);
+            })
+          })
+        })
+      })
+    }
+  }
+  if (req.body.type === 'file') {
+    fs.appendFile(path, `${req.body.data}`, err => {
       if (err) console.log(err);
       console.log('File created');
-      res.json(body);
+      res.json(req.body);
     })
   }
-  if (body.type === 'folder') {
-    fs.mkdir(res.locals.path + '/' + body.name, err => {
+  if (req.body.type === 'folder') {
+    fs.mkdir(path, err => {
       if (err) console.log(err);
       console.log('Directory created')
-      res.json(body);
+      res.json(req.body);
     })
   }
 })
 
 //DELETE file/directory
 
-router.delete('/:username', adjustUserPath, ({body}, res) => {
-  fs.rm(`${res.locals.path}/${body.name}`, { recursive: true }, err => {
+router.delete('/:username/:folder?', adjustUserPath, (req, res) => {
+  console.log(req.params);
+  fs.rm(`${res.locals.path}/${req.params.folder||''}/${req.body.name}`, { recursive: true }, err => {
     if (err) console.log(err);
     console.log('Item deleted');
-    res.json(body);
-    })
+    res.json(req.body);
+  })
 })
 
-//PUT file name
+//PUT rename file 
 
-router.put('/:username', adjustUserPath, ({ body }, res) => [
-  fs.rename(`${res.locals.path}/${body.oldName}`, `${res.locals.path}/${body.newName}`, err => {
+router.put('/:username/:folder?', adjustUserPath, (req, res) => {
+  fs.rename(`${res.locals.path}/${req.params.folder||''}/${req.body.oldName}`, `${res.locals.path}/${req.params.folder||''}/${req.body.newName}`, err => {
     if (err) console.log(err);
     console.log('File renamed!');
-    res.json(body);
+    res.json(req.body);
   })
-])
+})
+
+//PUT move file
+
+// router.put('/:username/:folder?/:file', adjustUserPath, (req, res) => {
+//   fs.rename(`${res.locals.path}/${req.params.folder||''}/${req.params.file}`,)
+// })
+
+
 
 module.exports = router;
 
