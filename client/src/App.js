f469639ef31c4741450b001b5ffa36a97d9cfdc8
hilma <jonathanco999@gmail.com>
2023-01-16 16:30:18 +0200

    login component and routes beta

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import FilesList from './components/filesList'
+import { createContext } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Login from './components/login';
+
+export const UserContext = createContext();
 
 function App() {
   return (
-    <FilesList/>
+    <UserContext.Provider >
+      <Routes>
+        <Route
+          path='/'
+          element={<Login />}
+        />
+      </Routes>
+      <Route
+        path='/drive'
+      element={<FilesList />}
+      />
+    </UserContext.Provider>
   );
 }
 
